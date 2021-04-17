e820477aeb602314759f957edd7380baabe1eaa8
Marius Brennfleck <marius.brennfleck@student.kit.edu>
2021-04-17 03:37:15 +0200

    first js added

---
+++ demo.js
@@ -0,0 +1,13 @@
+/******************************************************/
+/****************** Beschleunigungssensoren **************************/
+window.ondevicemotion = function(event) { 
+	var ax = event.accelerationIncludingGravity.x
+	var ay = event.accelerationIncludingGravity.y
+	var az = event.accelerationIncludingGravity.z
+
+	document.querySelector("#acc").innerHTML = "X = " + ax + "<br>" + "Y = " + ay + "<br>" + "Z = " + az;
+}
+
+window.addEventListener("deviceorientation", function(event) {
+	document.querySelector("#mag").innerHTML = "alpha = " + event.alpha + "<br>" + "beta = " + event.beta + "<br>" + "gamma = " + event.gamma;
+}, true);
